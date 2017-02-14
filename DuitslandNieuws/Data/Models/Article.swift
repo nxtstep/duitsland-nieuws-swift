dd6302273971e1b3ab93bb61c650b7b67b7b092c
Arjan Duijzer <arjan@nxtstep.nl>
2017-02-14 17:03:09 +0100

    Adding Cuckoo for Mock/Stubbing dependecies in Test-cases

---
+++ DuitslandNieuws/Data/Models/Article.swift
@@ -0,0 +1,10 @@
+//
+// Created by Arjan Duijzer on 14/02/2017.
+// Copyright (c) 2017 SuperSimple.io. All rights reserved.
+//
+
+import Foundation
+
+struct Article {
+    let articleId: String
+}
---
+++ DuitslandNieuws/Data/Repositories/Article/ArticleRepository.swift
@@ -0,0 +1,22 @@
+//
+// Created by Arjan Duijzer on 14/02/2017.
+// Copyright (c) 2017 SuperSimple.io. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class ArticleRepository {
+    let cache: ArticleCache
+    let cloud: ArticleCloud
+
+     init(_ cache: ArticleCache, _ cloud: ArticleCloud) {
+         self.cache = cache
+         self.cloud = cloud
+     }
+
+    func list() -> Observable<[Article]> {
+        return Observable.empty()
+    }
+    
+}
