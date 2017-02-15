961f810a9f2e0dea1de94d1e21ba62c6f794fd8e
Arjan Duijzer <arjan@nxtstep.nl>
2017-02-15 16:07:03 +0100

    ArticleRepository adding CRUD methods: get, save, delete, refresh
    - get Article
    - refresh
    - save and clear

--- DuitslandNieuws/Data/Repositories/Article/ArticleRepository.swift
+++ DuitslandNieuws/Data/Repositories/Article/ArticleRepository.swift
@@ -10,17 +10,55 @@ class ArticleRepository {
     let cache: ArticleCache
     let cloud: ArticleCloud
 
+    static let defaultPageSize = 10
+
     init(_ cache: ArticleCache, _ cloud: ArticleCloud) {
         self.cache = cache
         self.cloud = cloud
     }
 
-    func list() -> Observable<[Article]> {
-        return cache.listAll()
-                .ifEmpty(switchTo: cloud.list()
+    func list(_ page: Int = 0, pageSize size: Int = defaultPageSize) -> Observable<[Article]> {
+        return cache.list(page, pageSize: size)
+                .ifEmpty(switchTo: cloud.list(0, pageSize: size)
                         .flatMap { [unowned self] in
                             self.cache.save($0)
                         })
     }
 
+    func get(articleId id: String) -> Observable<Article> {
+        return cache.get(id)
+                .ifEmpty(switchTo: cloud.fetch(id: id)
+                        .flatMap { [unowned self] in
+                            self.cache.save($0)
+                        })
+    }
+
+    func refresh(_ pageSize: Int) -> Observable<[Article]> {
+        return cache.deleteAll()
+                .flatMap { [unowned self] (_) in
+                    self.list(0, pageSize: pageSize)
+                }
+    }
+
+    func save(_ article: Article) -> Observable<Article> {
+        return cache.save(article)
+    }
+
+    func save(list articles: [Article]) -> Observable<[Article]> {
+        return cache.save(articles)
+    }
+
+    func delete(_ article: Article) -> Observable<Article> {
+        return cache.delete(article)
+    }
+
+    func clearCaches() -> Observable<Void> {
+        return cache.deleteAll().flatMap { _ in
+            Observable<Void>.create { observer in
+                observer.onCompleted()
+
+                return Disposables.create()
+            }
+        }
+    }
 }
